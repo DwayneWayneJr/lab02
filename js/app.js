7d11b43419a44033b59fe054d1b34dcb29a75fcd
DwayneWayneJr <dwaynejgrant@gmail.com>
2019-09-11 13:45:11 -0700

    created folder for js

---
+++ js/app.js
@@ -0,0 +1,44 @@
+//prompt asks for name and says hello to the user
+var userName = prompt('What\'s your name? ');{
+  alert('Check me out ' + userName + '!!!');
+}
+
+var whereFrom = prompt('Am I from Washington? ').toLowerCase();
+
+if (whereFrom === 'no'){
+  console.log('This answer is a no.'); alert('BINGO ' + name + '.');
+} else{
+  alert('That\'s a no-go at this station');
+}
+
+var kids = prompt('Do I have kids? ').toLowerCase();
+
+if (kids === 'yes'){
+  console.log('This answer is a yes.'); alert('Cha Chiiiiing' + '!!!');
+} else{
+  alert('Not quite the response I was hoping for.');
+}
+
+var pets = prompt('Do I have pets? ').toLowerCase();
+
+if (pets === 'no'){
+  console.log('This answer is a no.'); alert('Studied hard you have' + '.');
+} else{
+  alert('Again you must try my young Padawan.');
+}
+
+var instrument = prompt('Do I play an instrument? ').toLowerCase();
+
+if (instrument === 'yes'){
+  console.log('This answer is a yes.'); alert('I\'d call that a touchdown friend.');
+} else{
+  alert('No, just no.');
+}
+
+var hunt = prompt('Do I hunt? ').toLowerCase();
+
+if (hunt === 'yes'){
+  console.log('This answer is a yes.'); alert('I knew you knew ' + name + '.');
+} else{
+  alert('Please try again.');
+}
